08f9c151f97f803cc4149396973f056fef38df4e
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-04-28 16:47:49 +0600

    C#

---
+++ Exercise/WhatIsLinq/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WhatIsLinq
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SampleDataContext sampleData= new SampleDataContext();
+
+            //GridView1.DataSource = from s in sampleData.Students
+            //                       where  s.Gender=="Male"
+            //                       select s;
+
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            GridView1.DataSource = from n in numbers
+                                   where (n%2==0)
+                                   select n;
+            GridView1.DataBind();
+        }
+    }
+}
